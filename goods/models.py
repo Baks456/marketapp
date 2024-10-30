9589a64c30279c94f5cc57b0440bc07e43a3df27
Baks <baks-p13@yandex.ru>
2024-10-30 17:42:52 +0300

    Пагинация вариант 1, функцией

--- goods/models.py
+++ goods/models.py
@@ -38,6 +38,7 @@ class Products(models.Model):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+        ordering = ['-price', 'name']
 
     def display_id(self):
         return f'{self.id:06}'
--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 # Create your views here.
 from goods.models import Products
 
 
-def catalog(request):
-    goods = Products.objects.all().order_by('-price')
+def catalog(request, cat_slug, page=1):
+    if cat_slug == 'all':
+        goods = get_list_or_404( Products.objects.order_by('-price'))
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=cat_slug))
+
+    paginator = Paginator(goods, 3)
+    curr_page = paginator.page(page)
     context = {
         'title': 'Каталог товаров',
-        'goods': goods,
+        'goods': curr_page,
+        'cat_slug': cat_slug,
     }
     return render(request, 'goods/catalog.html', context)
 
