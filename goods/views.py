affed3e2446ceae78764840eaefb0d925420b471
Baks <baks-p13@yandex.ru>
2024-10-30 15:33:48 +0300

    Добавлены slug для товаров

--- goods/views.py
+++ goods/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
-from goods.models import Categories, Products
+from goods.models import Products
 
 
 def catalog(request):
-
     goods = Products.objects.all().order_by('-price')
     context = {
         'title': 'Каталог товаров',
-
         'goods': goods,
     }
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
+
+def product(request, product_slug):
+    good = Products.objects.get(slug=product_slug)
     context = {
-        'title': 'Страница продуктов',
-        'content': 'Baks Hi-Tech store'
+        'good': good,
     }
     return render(request, 'goods/product.html', context)
--- marketapp/urls.py
+++ marketapp/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('baseitems.urls', namespace='baseitems')),
 
-    path('catalog/', include('goods.urls', namespace='goods')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
 
 if DEBUG :
