8851a5aeca913468300fb7b2febc9136d43cfe71
Baks <baks-p13@yandex.ru>
2024-11-01 23:13:33 +0300

    правки формы

--- users/forms.py
+++ users/forms.py
@@ -4,8 +4,8 @@ from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Имя пользователя', required=True)
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Пароль', required=True)
+    # username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':"Введите ваше имя пользователя или Email"}), label='Имя пользователя', required=True)
+    # password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Пароль', required=True)
 
     class Meta:
         model = get_user_model()
